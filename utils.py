141a1b3318f9532d173e3a24ebf9cb411f5964ec
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-10-29 15:46:25 -0300

    feat: Build load_dataset function

--- utils.py
+++ utils.py
@@ -1,29 +1,13 @@
 import pandas as pd
 
-# Visualization
-import matplotlib.pyplot as plt
-import seaborn as sns
-%matplotlib inline
-
 
 def load_dataset():
-    df = pd.read_csv('datasets/baseSCL2017.csv', dtype={'Vlo-I': str, 'Vlo-O': str})
+    df = pd.read_csv('datasets/baseSCL2017.csv',
+                     parse_dates=['Fecha-I', 'Fecha-O'],
+                     dtype={'Vlo-I': str, 'Vlo-O': str})
     df.drop(columns=['Unnamed: 0'], inplace=True)
     for col in df.columns:
         if df[col].nunique() < 100:
             df[col] = df[col].astype('category')
+    df.set_index('Fecha-I', inplace=True)
     return df
-
-
-df.info()
-
-df['DIANOM'] = df['DIANOM'].astype(str)
-df.info()
-df['DIANOM'].value_counts()
-
-
-df = pd.read_csv('datasets/baseSCL2017.csv', dtype={'Vlo-I': str, 'Vlo-O': str})
-df.drop(columns=['Unnamed: 0'], inplace=True)
-df['DIANOM'].value_counts()
-_ = plt.hist(df['DIANOM'].value_counts())
-plt.show()
